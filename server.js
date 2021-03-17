bdfc2771b5470efb953832ccc88f99caa04269a6
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2021-03-16 18:00:16 -0600

    once again

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
-
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 const contactEmail = nodemailer.createTransport({
     host: "smtp.gmail.com",
