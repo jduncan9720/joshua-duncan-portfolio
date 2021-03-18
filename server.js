0c54c2227f0ea83029a6d91ee2c5c45cdb6579b1
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2021-03-17 20:53:43 -0600

    rebuild

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const PORT = process.env.PORT || 5000
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const app = express();
@@ -7,8 +8,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
@@ -48,4 +52,8 @@ const contactEmail = nodemailer.createTransport({
         res.json({ status: "sent" });
       }
     });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
   });
