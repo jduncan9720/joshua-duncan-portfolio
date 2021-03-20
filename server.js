c17950632045e316d255c7075f43b813ec5ae32d
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2021-03-20 09:35:11 -0600

    pushing again

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 const app = express();
+const path = require('path')
 
 app.use(cors());
 app.use(express.json());
